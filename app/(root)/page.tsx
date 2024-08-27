96f57c6d7121fd3bc294f1045cb20add70fa4ba8
harshBansal47 <harshbansal472000@gmail.com>
2024-08-27 18:35:20 +0530

    inital home page design included

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,17 @@
+"use client"
+import Wrapper from "@/components/layout/Wrapper";
+import { Metadata } from "next";
+import HomeMain from '@/components/home/index';
+import { Provider } from "react-redux";
+import {store} from "@/store/store"
 
 
 export default function HomePage(){
-    return(<>
-    Test</>)
+   return(
+    <Wrapper>
+        <Provider store={store}>
+            <HomeMain/>
+        </Provider>
+    </Wrapper>
+   )
 }
